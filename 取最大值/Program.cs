54240a5cccb27ec608ed9be8ed8d7372e3a17e09
自由之翼 <@LAPTOP-0QVJ42G4>
2019-02-20 21:08:29 +0800

    取最大值

--- 取最大值/Program.cs
+++ 取最大值/Program.cs
@@ -8,18 +8,23 @@ namespace 取最大值
 
         {
             int[] array = new int[] { 6, 3, 7, 2, 5, 9 };
+            Console.WriteLine("数组：6，3，7，2，5，9");
 
-           int max = array[0];
+            int max = array[0];
+            Console.WriteLine($"最大值的初始值为:{max}");
             for (int i = 1; i < array.Length; i++)
-
             {
                 if (array[i] > max)
                 {
-                    max = array[i];
+                    Console.WriteLine($"第{i}次比较，{array[i]}>{max},目前最大值为{array[i]}");max = array[i];
+
+                }
+                else
+                {
+                    Console.WriteLine($"第{i}次比较，{array[i]}<={max},最大值不变"+max);
                 }
             }
-            Console.WriteLine(max);
-            Console.WriteLine(@"飞哥最""帅""");
+            Console.WriteLine($"最后的结果，最大数字是：" + max);
             Console.Read();
 
 
