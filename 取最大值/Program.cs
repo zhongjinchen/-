119614e8d12d5eea1efd2819b1d985931dc66f89
自由之翼 <@LAPTOP-0QVJ42G4>
2019-02-20 11:48:10 +0800

    添加项目文件。

---
+++ 取最大值/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 取最大值
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[] array = new int[] { 6, 3, 7, 2, 5, 9 };
+
+           int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine(max);
+           
+            Console.Read();
+
+
+        }
+    }
+}
